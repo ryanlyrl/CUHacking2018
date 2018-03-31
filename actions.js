aa5531ec39aa1da9ba684c571196736db3898f7c
Ryan Ly <ryanly.rl1@gmail.com>
2018-03-31 14:39:54 -0400

    Initial commit

---
+++ actions.js
@@ -0,0 +1,13 @@
+function clickButton(text){
+	$(document).ready(function(){
+		console.log("jQuery Extension running...");
+	});
+}
+
+function scroll(direction){ //1 down, -1 up
+	window.scrollBy(window.innerHeight * direction);
+}
+
+function enterText(){
+	
+}
