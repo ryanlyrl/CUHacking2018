babfff65e0dff88042534a9a49e339346211cdda
Stephanie Wang <stephaniewang@dhcp-46-108.cu-wireless-faculty.carleton.ca>
2018-03-31 20:48:26 -0400

    before extension

--- microphone.js
+++ microphone.js
@@ -1,8 +1,18 @@
 var recognizing = false;
 var ignore_onend;
 var start_timestamp;
+var final_transcript = '';
+var commands = new Map();
+
+commands.set("click",clickButton);
+commands.set("scroll",scroll);
+commands.set("switch",switchTab);
+commands.set("open", openTab);
+commands.set("close",closeTab);
+commands.set("maximize", maximize)
+commands.set("back", prevPage);
 if (!('webkitSpeechRecognition' in window)) {
-  upgrade();
+  //upgrade();
 } else {
   start_button.style.display = 'inline-block';
   var recognition = new webkitSpeechRecognition();
@@ -18,17 +28,24 @@ if (!('webkitSpeechRecognition' in window)) {
       return;
     }
     start_img.src = 'mic.gif';
-    if (!final_transcript) {
+    /*if (!final_transcript) {
       return;
-    }
+    }*/
     showInfo('');
   };
   recognition.onresult = function(event) { //add to string
     var interim_transcript = '';
     for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if(event.results[i].isFinal){
+        final_transcript += event.results[i][0].transcript;
+      }
+      else{
         interim_transcript += event.results[i][0].transcript;
+      } 
     }
+    final_span.innerHTML = linebreak(final_transcript);
     interim_span.innerHTML = linebreak(interim_transcript); //outputs
+    doAction(final_transcript);
   };
 }
 
@@ -46,7 +63,9 @@ function startButton(event) {
   recognition.start();
   ignore_onend = false;
   interim_span.innerHTML = '';
+  final_span.innerHTML = '';
   start_img.src = 'mic-slash.gif';
+
   showInfo('info_allow');
   start_timestamp = event.timeStamp;
 }
@@ -60,5 +79,18 @@ function showInfo(s) {
     info.style.visibility = 'visible';
   } else {
     info.style.visibility = 'hidden';
+  }
+}
+
+function doAction(s){
+  var temp = s.split(" ")
+
+  for(var i=0; i< temp.length; i++){
+    console.log(temp);
+    if(commands.get(temp[i]) != undefined){
+      console.log("hi");
+      commands.get(temp[i])();
+        return;
+    }
   }
 }
